befa2d6fd05d8e5a8bd9febbba0a4073d6353ce4
Kalyan Bhattarai <bhabishwor70@gmail.com>
2025-07-31 15:18:19 +0545

    Simplify chat gateway: remove username logic, use socket IDs only

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -50,7 +50,11 @@ import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
   cors: {
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+    origin: [
+      'http://127.0.0.1:5500',
+      'http://localhost:5500',
+      'https://1kalyan.github.io',
+    ],
     methods: ['GET', 'POST'],
   },
 })
